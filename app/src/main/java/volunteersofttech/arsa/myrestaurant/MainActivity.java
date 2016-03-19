c301937701b2e66384f4dfe7eef7e74c76f383c6
arsa <arsa_ko@hotmail.com>
2016-03-19 11:46:17 +0700

    Add Value SqLite

--- app/src/main/java/volunteersofttech/arsa/myrestaurant/MainActivity.java
+++ app/src/main/java/volunteersofttech/arsa/myrestaurant/MainActivity.java
@@ -17,8 +17,16 @@ public class MainActivity extends AppCompatActivity {
         //Request Sqllite
         myManage = new MyManage(this);
 
+        //test add vale
+        testaddvalue();
+
 
 
     }//main method "method อันแรกที่ทำงาน oncreate"
+
+    private void testaddvalue() {
+        myManage.addValue(1, "user", "pass", "name");
+        myManage.addValue(2, "food", "price", "source");
+    }
 
 }//main class
--- app/src/main/java/volunteersofttech/arsa/myrestaurant/MyManage.java
+++ app/src/main/java/volunteersofttech/arsa/myrestaurant/MyManage.java
@@ -1,5 +1,6 @@
 package volunteersofttech.arsa.myrestaurant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,11 +11,23 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class MyManage {
 
     //ประกาศตัวแปร
-    private  MyOpenHelper myOpenHelper ;
-    private SQLiteDatabase WriteSqLiteDatabase,readSqLiteDatabase;
+    private MyOpenHelper myOpenHelper;
+    private SQLiteDatabase WriteSqLiteDatabase, readSqLiteDatabase;
+
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_user = "User";
+    public static final String column_password = "Password";
+    public static final String column_name = "Name";
+
+    public static final String food_table = "foodTABLE";
+    public static final String column_food = "Food";
+    public static final String column_price = "Price";
+    public static final String column_source = "Source";
 
 
-    public  MyManage(Context context) {;
+    public MyManage(Context context) {
+        ;
 
         //create & connect sqllite
         myOpenHelper = new MyOpenHelper(context);
@@ -23,6 +36,46 @@ public class MyManage {
 
 
     }//Construtor
+
+    public long addValue(int intTable,
+                         String strcolumn2,
+                         String strcolumn3,
+                         String strcolumn4) {
+        ContentValues contentValues = new ContentValues();
+
+        long mylong = 0;
+
+        switch (intTable) {
+            case 1:
+                //for usertable
+                contentValues.put(column_user, strcolumn2);
+                contentValues.put(column_password, strcolumn3);
+                contentValues.put(column_name, strcolumn4);
+
+                mylong = WriteSqLiteDatabase.insert(user_table, null, contentValues);
+
+                break;
+            case 2:
+                //for foodtable
+                contentValues.put(column_food, strcolumn2);
+                contentValues.put(column_price, strcolumn3);
+                contentValues.put(column_source, strcolumn4);
+
+                mylong = WriteSqLiteDatabase.insert(food_table, null, contentValues);
+
+
+                break;
+
+        }
+
+        return mylong;
+
+
+
+    }
+
+
+
 
 
 } //Main Class
